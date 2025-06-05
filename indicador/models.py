7d095437187dd093c367987c4736b026028de599
SofiaDyLaFuente <sofia.dylafuente@gmail.com>
2025-06-05 16:51:55 -0300

    feat: Model

---
+++ indicador/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.conf import settings
+
+
+class PalavraChave(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta: 
+        verbose_name = "palavra chave"
+        verbose_name_plural = "palavras chave"
+
+
+class Etiqueta(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, unique=True) 
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "etiqueta"
+        verbose_name_plural = "etiquetas"
+
+
+class Indicador(models.Model):
+
+    QUALIFICACAO_CHOICES = (
+        ("A", "NÃO SEI 1"),
+        ("B", "não sei mas em minusculo"),
+    )
+
+    PERIODICIDADE_CHOICES = (
+        ("diariamente", "Diariamente"),
+        ("semanalmente", "Semanalmente"),
+        ("mensalmente", "Mensalmente"),
+        ("anualmente", "Anualmente"),
+        ("bianualmente", "Bianualmente"),
+        ("a cada 3 anos", "A cada 3 anos"),
+        ("a cada 5 anos", "A cada 5 anos"),
+    )
+
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, unique=True)
+    atualizado_por = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="atualizado_por", on_delete=models.CASCADE
+    )
+    destaque = models.BooleanField(default=False)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    ultima_atualizacao = models.DateTimeField(auto_now=True)
+    qualificacao = models.CharField(max_length=1, choices=QUALIFICACAO_CHOICES)
+    periodicidade = models.CharField(max_length=20, choices=PERIODICIDADE_CHOICES)
+    palavra_chave = models.ManyToManyField(PalavraChave)
+    responsavel_tecnico = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="responsavel_tecnico", on_delete=models.CASCADE
+    )
+    etiqueta = models.ManyToManyField(Etiqueta)
+    conceito = models.TextField(max_length=500, blank=True)
+    metodo_de_calculo = models.TextField(max_length=500, blank=True)
+    interpretacao = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "indicador"
+        verbose_name_plural = "indicadores"    
+    
+
+
+
