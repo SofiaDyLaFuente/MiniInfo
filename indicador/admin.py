ac89b1b3886ae10b14846f5c2d0d869250f447e2
SofiaDyLaFuente <sofia.dylafuente@gmail.com>
2025-06-05 16:52:18 -0300

    feat: Admin

---
+++ indicador/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from indicador.models import PalavraChave, Etiqueta, Indicador
+
+
+@admin.register(PalavraChave)
+class PalavraChaveAdmin(admin.ModelAdmin):
+    list_display = ["nome"]
+
+@admin.register(Etiqueta)
+class EtiquetaAdmin(admin.ModelAdmin):
+    list_display = ["nome"]
+
+@admin.register(Indicador)
+class IndicadorAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'nome',
+        'destaque',
+        'criado_em',
+        'ultima_atualizacao',
+        'qualificacao',
+        'periodicidade',
+        'conceito',
+        'metodo_de_calculo',
+        'interpretacao'
+    ]
+    
